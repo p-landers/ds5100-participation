523a4104d2d15af61ade9c19dbb4604553302f51
p-landers <planders416@gmail.com>
2023-10-24 20:25:21 -0400

    Added all required files for participation

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='this was made for participation in class',
+    author='Peter Landers',
+    license='MIT',
+    packages=['package']
+     )
